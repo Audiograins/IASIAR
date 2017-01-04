ed438a26ade55a23e3e0fe7e32811a333678aba0
Matt Gallagher <mattbgallagher@gmail.com>
2017-01-03 16:24:54 -0800

    Default Single View App with AudioKit.framework included

---
+++ IASIAR/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  IASIAR
+//
+//  Created by Gallagher, Matthew on 1/3/17.
+//  Copyright © 2017 Gallagher, Matthew. All rights reserved.
+//
+
+import UIKit
+import AudioKit
+
+class ViewController: UIViewController {
+
+    var oscillator = AKOscillator()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+# IASIAR
+
+Inspired by Alvin Lucier's "I Am Sitting In A Room", this app will capture or load a short recording and capture or load an impulse response and recursively convolve the captured audio with the IR to somewhat digitally recreate Lucier's analog process
